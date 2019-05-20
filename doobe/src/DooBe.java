7efcf202f12c9aa00e1f5436ed5e7cfc3b3034bf
creckaim <creckaim@gmail.com>
2019-05-20 19:38:54 +0400

    создание программы DooBe из учебника Изучаем Java. Syerra Beyts

---
+++ doobe/src/DooBe.java
@@ -0,0 +1,15 @@
+package src;
+
+public class DooBe {
+	public static void main(String[] args) {
+		int x=1;
+		while(x<3) {
+			System.out.print("Doo");
+			System.out.print("Be");
+			x=x+1;
+		}
+		if(x==3) {
+			System.out.print("Do");
+		}
+	}
+}
